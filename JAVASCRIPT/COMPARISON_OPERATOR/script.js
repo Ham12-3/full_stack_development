02bf65c804293f154e62f33a012126bd47d55602
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-07-02 20:54:25 +0000

    update

---
+++ JAVASCRIPT/COMPARISON_OPERATOR/script.js
@@ -0,0 +1,37 @@
+// Comparison Operators in JavaScript are used to compare two values. It returns a boolean value either true or false.
+
+let aageOfJohn = 25;
+
+let ageOfDoe = 30;
+
+let userInput = "25";
+
+console.log(aageOfJohn == userInput); // true
+
+// Using the ===
+
+console.log(aageOfJohn === userInput); // false
+
+// using the != operator
+
+console.log(aageOfJohn != ageOfDoe); // true
+
+// using the !== operator
+
+console.log(aageOfJohn !== ageOfDoe); // true
+
+// Using the > operator
+
+console.log(aageOfJohn > ageOfDoe); // false
+
+// Using the < operator
+
+console.log(aageOfJohn < ageOfDoe); // true
+
+// Using the >=
+
+console.log(aageOfJohn >= ageOfDoe); // false
+
+// using the <= operator
+
+console.log(aageOfJohn <= ageOfDoe); // true
