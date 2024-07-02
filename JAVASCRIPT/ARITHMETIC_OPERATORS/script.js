0e326ff5c50bfa604ce1f81fcfe8dc4b39995979
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-07-02 17:35:11 +0000

    update

---
+++ JAVASCRIPT/ARITHMETIC_OPERATORS/script.js
@@ -0,0 +1,41 @@
+// Basic Arithmetic Operations
+
+let a = 5;
+
+let b = 2;
+
+const results = a + b;
+
+// Subtraction
+
+const subtract = a - b;
+
+// Multiplication
+
+const multiply = a * b;
+
+// Division
+
+const divide = a / b;
+
+// Using Modulus to find Remainder
+
+const remainder = a % b; // 1
+
+// Increment
+
+let number = 10;
+
+number++; // 11
+
+// Decrement
+
+number--; // 10
+
+// COmpound assignment operators
+
+let score = 10;
+
+score += 5; // 15
+
+score -= 5; // 10
