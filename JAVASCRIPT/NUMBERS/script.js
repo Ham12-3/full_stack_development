57b2fe34dd5aa8e3a45f5176834a789f30721171
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-07-04 19:52:49 +0000

    update

---
+++ JAVASCRIPT/NUMBERS/script.js
@@ -0,0 +1,27 @@
+// Converting a Number to a String in JavaScript
+
+const age = 30;
+
+const ageSt = age.toString();
+
+console.log(typeof ageSt);
+
+// Dsiplaying a price
+
+const price = 30;
+
+const displayPrice = "The price is " + price + " dollars";
+
+console.log(displayPrice);
+
+// Using toString() with different bases
+
+const decimalNumber = 10;
+
+const binaryNumber = decimalNumber.toString(2);
+
+// Working withBigInt
+
+const ngiIntNum = BigIng(9007199254740991);
+
+console.log(bigIntNum.toString());
