340049b7a8b2bc6a5d72c00c9fa6801c45fdf2b1
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-07-06 13:29:49 +0000

    update

--- JAVASCRIPT/ARRAY_TRANSVERSAL/script.js
+++ JAVASCRIPT/ARRAY_TRANSVERSAL/script.js
@@ -57,3 +57,68 @@ numbers.forEach(function (number) {
 });
 
 console.log(square);
+
+// The map( ) method creates a new array with the results of calling a provided function on every element in the calling array.
+
+// Mapping map() method
+
+const numbers1 = [1, 2, 3, 4, 5];
+
+const newNUmArr = numbers1.map(function (number) {
+  return number * number;
+});
+
+console.log(newNUmArr); // [1, 4, 9, 16, 25]
+
+// Using map to convert an array of strings to uppercase
+
+const words = ["apple", "banana", "mango", "orange", "papaya"];
+
+const upperCaseWords = words.map(function (word) {
+  return word.toUpperCase();
+});
+
+console.log(upperCaseWords); // ['APPLE', 'BANANA', 'MANGO', 'ORANGE', 'PAPAYA']
+
+// Using filter to filter out elements from an array
+
+const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// filter out even numbers
+
+const evenNumbers = numbers2.filter(function (number) {
+  return number % 2 === 0;
+});
+
+const gender = ["male", "female"];
+
+const maleArr = gender.filter(function (gender) {
+  return gender === "male";
+});
+
+console.log(maleArr); //   ["male"]
+
+// Using filter to remove falsy values from an array
+
+const mixedArr2 = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  0,
+  "",
+  undefined,
+  null,
+  NaN,
+  false,
+];
+
+const filteredArr = mixedArr2.filter(Boolean);
+
+console.log(filteredArr); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
