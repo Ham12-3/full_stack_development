747363d516f2add65744bee99a39d4f4a45038f9
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-07-09 17:50:03 +0100

    update

--- JAVASCRIPT/JAVASCRIPT_FUNCTIONS/script.js
+++ JAVASCRIPT/JAVASCRIPT_FUNCTIONS/script.js
@@ -91,7 +91,38 @@ checkEvenOrOdd(15); // Odd
 
 // Function with parameters and arguments
 
-const greetings = function(name) {
+const greetings = function(name) { // name is a parameter
   console.log(`Hello ${name}`);
 }
 
+
+greetings("John"); // Hello John (argument)
+
+
+// Function to add two numbers 
+
+const result = function(num1, num2) {
+  console.log(num1 + num2);
+}
+
+result(10, 20); // 30
+
+result(90000,100000); // 190000
+
+
+// Basic Default Parameters 
+const greet1 = function(name = "John", age = 30) {
+  console.log(`Hello ${name}, you are ${age} years old`);
+}
+
+greet1(); // Hello John, you are 30 years old
+
+
+greet1("Doe", 40); // Hello Doe, you are 40 years old
+// Default Parameter with expression 
+
+const multiply = function(num1, num2 = 10) {
+  console.log(num1 * num2);
+}
+
+multiply(5); // 50
