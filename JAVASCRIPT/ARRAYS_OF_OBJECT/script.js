00af71316c3d3823e4705a346fab92a3021a33ff
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-07-10 14:21:50 +0100

    update

--- JAVASCRIPT/ARRAYS_OF_OBJECT/script.js
+++ JAVASCRIPT/ARRAYS_OF_OBJECT/script.js
@@ -31,3 +31,47 @@ const incrementAge = (personObj) => {
 const bob = {name: "Bob", age: 30};
 
 incrementAge(bob);
+
+
+// Objects in Arrays 
+
+// Sceanrio: managing a list of students where each of student is an object with properties like id , name and age 
+
+const students = [
+
+    {id: 1, name: "Alice", age: 20},
+    {id: 2, name: "Bob", age: 25},
+
+{id: 3, name: "Charlie", age: 30},
+
+{id: 4, name: "David", age: 35}
+, {id: 5, name: "Eve", age: 40}
+];
+
+// Access student name
+
+const student1 = students[0].name;
+
+
+// Add new student 
+
+const addStudent = (studentObj) => {
+    students.push(studentObj);
+}
+
+addStudent({id: 6, name: "Frank", age: 45});
+
+// Update a student 
+
+const updateStudentGrade = (id,newGrade) => {
+    // find the student by id 
+    const studentFound = students.find(student => student.id === id);
+    if(studentFound) {
+        studentFound.grade = newGrade;
+
+    } else {
+        console.log("Student not found");
+    }
+}
+
+updateStudentGrade(2, "A"); // {id: 2, name: "Bob", age: 25, grade: "A"}
