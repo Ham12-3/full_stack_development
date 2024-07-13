c9eb5752a819bd5dd60b3e0b2cc11c9b617c1696
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-07-13 20:23:06 +0100

    update

---
+++ DOM/inlineEventHandler/script.js
@@ -0,0 +1,43 @@
+// Mouse event 
+
+function clickHandler() {
+    console.log('Button clicked')
+}
+
+
+function dbClickHandler() {
+console.log("button double clicked")
+}
+
+function onMouseHoverHandler()  {
+    console.log("mouse was hovered")
+}
+
+function onMouseOutHandler() {
+    console.log("mouse was out")
+}   
+
+
+function onkeydownHandler() {
+    console.log("key was pressed")
+}
+
+function onkeyupHandler() {
+    console.log("key was released")
+}
+
+function onkeypressHandler() {
+    console.log("key was pressed and released")
+}
+
+function onsubmitHandler() {
+    console.log("form was submitted")
+}
+
+function onfocusHandler() {
+    console.log("input was focused")
+}
+
+function onblurHandler() {
+    console.log("input was blurred")
+}
