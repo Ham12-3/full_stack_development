d54957a19ff342658fa0fb81ac69113c1e92843b
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-07-31 23:10:39 +0100

    update

---
+++ BACKEND/expressjs/rendering-asset/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const path =require('path');
+
+const app = express();
+
+const PORT =8082
+
+// Serve the static files/folder 
+
+app.use(express.static(path.join(__dirname,"public"))) 
+
+
+// Render Home page /route 
+
+app.get("/", (req,res)=> {
+    res.sendFile(path.join(__dirname,"views","home.html"))
+
+})
+
+// Render the about page 
+
+app.get("/about", (req,res)=> {
+res.sendFile(path.join(__dirname,"views","about.html"))
+})
+
+// Render the contact page 
+
+app.get("/contact", (req,res)=> {
+    res.sendFile(path.join(__dirname,"views","contact.html"))
+})
+
+
+// Render the gallery page 
+
+app.get("/gallery", (req,res)=> {
+    res.sendFile(path.join(__dirname,"views","gallery.html"))
+})
+
+// Start the server 
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
