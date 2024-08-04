4daa36f7158f38c717c3a22d33d7c3c7f187e96c
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-08-04 05:08:04 +0100

    update

--- BACKEND/MONGODB/getting-started/server.js
+++ BACKEND/MONGODB/getting-started/server.js
@@ -1,47 +1,41 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+// require dotenv
+require("dotenv").config();
 
-const {MongoClient, ServerApiVersion} =require("mongodb");
-const { version } = require('os');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const PORT = 8082
+const PORT = 8082;
 
-// connect to mongoDB 
+// connect to mongoDB
 
-
-// 1. Create the client 
-
-const client = new MongoClient(
-    "mongodb://localhost:27017", {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            striict: true,
-            deprecationErrors: true
-    
-        }
-    }
-   
-)
-
-// Function to connect 
-
-const connectDb = async() => {
-    try {
-        await client.connect();
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB", error);
-    }
-}
-
-
-// Call the function 
-
-connectDb();
-// Start the server 
-
-app.listen(PORT, () => {
-    console.log('Server is running on PORT:', PORT);
+// 1. Create the client
+const uri = process.env.URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
+// Function to connect
+
+const connectDb = async () => {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+  }
+};
+
+// Call the function
+
+connectDb();
+// Start the server
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:", PORT);
+});
