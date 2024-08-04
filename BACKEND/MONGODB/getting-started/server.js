8f480c4f9d108365339b25fcb2b453c7359f2557
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-08-04 05:36:35 +0100

    update

--- BACKEND/MONGODB/getting-started/server.js
+++ BACKEND/MONGODB/getting-started/server.js
@@ -26,6 +26,52 @@ const connectDb = async () => {
   try {
     await client.connect();
     console.log("Connected to MongoDB");
+
+    // 1. Database name (school)
+    const database = client.db("masyntech");
+    // 2. Collection (books)
+    const students = database.collection("students");
+    // 3. Actual document (students)
+    // Using the insertOne()
+
+    // const results = await students.insertOne({
+    //   name: "Agnes",
+    //   age: 22,
+    //   subjects: ["Maths", "English", "Kiswahili"],
+    // });
+
+    // console.log(results);
+
+    // const results = await students.insertMany([
+    //   {
+    //     name: "Abdulhamid",
+    //     age: 24,
+    //     subjects: ["Maths", "English", "Kiswahili"],
+    //   },
+    //   {
+    //     name: "John",
+    //     age: 23,
+    //     subjects: ["Maths", "English", "Kiswahili"],
+    //   },
+    //   {
+    //     name: "Mary",
+    //     age: 24,
+    //     subjects: ["Maths", "English", "Kiswahili"],
+    //   },
+    // ]);
+
+    // Read operations in mongoDB
+
+    // const resultCursor = await students.find();
+
+    // const results = await resultCursor.toArray();
+    // console.log(results)
+
+    // Using the findOne() method
+    const result = await students.findOne({
+      age: 23,
+    });
+    console.log(result);
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
   }
