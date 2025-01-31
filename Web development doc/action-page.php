11e318e1f4e0c63ebff101699229f091ffbd6274
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2025-01-31 12:05:25 +0000

    Add files via upload

---
+++ Web development doc/action-page.php
@@ -0,0 +1,23 @@
+<?php
+
+  if (isset($_POST['submit'])){
+     $name = $_POST['name'];
+     $mailFrom = $_POST['mail'];
+     $message = $_POST['message'];
+
+     $email_from="adeyinka.olufodun@yahoo.co.uk";
+     $email_Subject= "New Form Submission";
+     $email_body="User Name: $name.\n".  
+                 "User Email: $visitor_email.\n".
+                 "User Message: $message.\n";
+
+
+     $to="adeyinka.olufodun@outlook.com;
+     $headers= 'From: $email_from  \r\n ";
+     $headers .= "Reply-To: $visitor_email \r\n";
+   
+   mail($to, $email_subject, $email_body, $headers);
+   header("Location:contact.html");
+     
+    
+   }
